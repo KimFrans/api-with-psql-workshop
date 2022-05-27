98a6565da5aff80ec75d47bc1be372b6025e55c1
Kim Frans <kimfrans34@gmail.com>
2022-05-27 16:10:32 +0200

    connectionstring, start, heroku

--- index.js
+++ index.js
@@ -1,94 +1,24 @@
-const supertest = require('supertest');
 const PgPromise = require("pg-promise")
 const express = require('express');
-const assert = require('assert');
-const fs = require('fs');
 require('dotenv').config()
 
 const API = require('./api');
-const { default: axios } = require('axios');
 const app = express();
 
 app.use(express.static('public'));
-// app.use('/images', express.static('images'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const pgp = PgPromise({});
-const db = pgp(DATABASE_URL);
-
-// const { DATABASE_URL } = process.env;
-// const cs = new ConnectionString(DATABASE_URL);
- 
-// function get_PostgreSQL_connection() {
-//    return {
-//        host: cs.hostname,
-//        port: cs.port,
-//        database: cs.path?.[0],
-//        user: cs.user,
-//        password: cs.password,
-//        ssl: DATABASE_URL.includes('localhost') ? false : {rejectUnauthorized: false},
-//        application_name: cs.params?.application_name
-//    };
-// }
- 
-// const pgp = PgPromise({});
- 
-// const db = pgp(get_PostgreSQL_connection());
-
-const garments = require('./garments.json');
+const db = pgp({connectionString: DATABASE_URL, 
+			max:30,
+		ssl: {rejectUnauthorized : false}
+});
 
 const PORT = process.env.PORT || 3000;
 
 API(app, db);
-
-app.get('/api/garments', function (req, res) {
-	
-	const gender = req.query.gender;
-	const season = req.query.season;
-
-	const filteredGarments = garments.filter(garment => {
-		// if both gender & season was supplied
-		if (gender != 'All' && season != 'All') {
-			return garment.gender === gender
-				&& garment.season === season;
-		} else if (gender != 'All') { // if gender was supplied
-			return garment.gender === gender
-		} else if (season != 'All') { // if season was supplied
-			return garment.season === season
-		}
-		return true;
-	});
-
-	// note that this route just send JSON data to the browser
-	// there is no template
-	
-
-	res.json({
-		garments: filteredGarments
-	});
-
-	// res.json({garments});
-
-});
-
-app.get('/api/garments/price/:price', function (req, res) {
-	const maxPrice = Number(req.params.price);
-	const filteredGarments = garments.filter(garment => {
-		// filter only if the maxPrice is bigger than maxPrice
-		if (maxPrice > 0) {
-			return garment.price <= maxPrice;
-		}
-		return true;
-	});
-
-	res.json({
-		garments: filteredGarments
-	});
-});
-
-
 
 app.listen(PORT, function () {
 	console.log(`App started on port ${PORT}`)
